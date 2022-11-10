48472c5d255d85fba2f9fb6250440b894c20f656
MohammadDallash <105324962+MohammadDallash@users.noreply.github.com>
2022-11-10 03:31:56 +0200

    test

---
+++ cmake-build-debug/CMakeFiles/CollegeProject.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CollegeProject.dir/main.cpp.obj"
+  "CMakeFiles/CollegeProject.dir/main.cpp.obj.d"
+  "CollegeProject.exe"
+  "CollegeProject.exe.manifest"
+  "CollegeProject.pdb"
+  "libCollegeProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CollegeProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/CollegeProject.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for CollegeProject.
+# This may be replaced when dependencies are built.
