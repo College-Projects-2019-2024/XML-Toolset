4f797a428ed842874fce6688ddc8aa1d9d7f9740
mohamed ahmed samir <105310173+Mohamed-samir-504@users.noreply.github.com>
2022-11-13 23:30:03 +0200

    created a separate cpp file for prettify

---
+++ Prettify_XML.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include "string"
+#include "stack"
+#include "vector"
+
+using namespace std;
+
+vector<string>answer;
+
+string insert_tab(string s,int n){
+    string str = " ";
+
+    for(int i = 0; i<4*n; i++){
+        s.insert(0,str);
+    }
+    return s;
+}
+
+void prettify(){
+    freopen("In.in","r",stdin);
+
+    string current_line;
+    stack<int> stac;
+    int count = 0;
+    string str;
+    while(getline(cin, current_line)){
+
+        while(current_line[0] == ' '){
+            current_line.erase(0,1);
+        }
+
+        if(current_line[0] == '<' && current_line[1] != '/'){
+            if(current_line.find('/') != -1){
+                answer.push_back(insert_tab(current_line,count));
+            }
+            else{
+                stac.push(count);
+                answer.push_back(insert_tab(current_line,count));
+                count++;
+            }
+
+        }
+        else if(current_line[0] == '<' && current_line[1] == '/'){
+            answer.push_back(insert_tab(current_line,stac.top()));
+            stac.pop();
+            count--;
+        }
+        else{
+            for(int i = 0; i<current_line.size()/50; i+=50)
+                str = current_line.substr(i,50);
+            answer.push_back(insert_tab(str,stac.top()+1));
+        }
+
+    }
+    for(auto & i : answer) cout<<i<<endl;
+}
---
+++ Prettify_XML.h
@@ -0,0 +1,9 @@
+#include "string"
+using namespace std;
+
+#ifndef COLLEGEPROJECT_PRETTIFY_XML_H
+#define COLLEGEPROJECT_PRETTIFY_XML_H
+void prettify();
+string insert_tab(string s,int n);
+
+#endif //COLLEGEPROJECT_PRETTIFY_XML_H
