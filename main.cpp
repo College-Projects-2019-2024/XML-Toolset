03585549c4a9aeb754f28c5f0c9d4f46991d9f6b
MohammadDallash <105324962+MohammadDallash@users.noreply.github.com>
2022-11-16 22:00:21 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,57 @@
 using namespace std;
 
 
+
+class hashUtil
+{
+public:
+
+    vector <string> values{"users","user","id","name","posts","post","body","topics","topic","followers","follower"};
+
+    vector <string> table;
+
+    hashUtil()
+    {
+        fori(50) table.push_back("null");
+
+        cout<<"hash utility class is done"<<endl;
+
+        for (auto x : values)
+        {
+            table [int (Hash_to_code('<'+x+'>'))]  = '<'+x+'>';
+        }
+        for (auto x : values)
+        {
+            table [int (Hash_to_code("</" + x +'>')) +23]  = "</" + x +'>';
+        }
+
+    }
+    char Hash_to_code(string s)
+    {
+        if (!(s.front() == '<' && s.back() == '>'))
+        {
+            return -1;
+        }
+        else
+        {
+            if (s[1]== '/')
+            {
+                return ( 2* (int(s[2]) - int(s[s.size()-2]) + 23) % 23)  ;
+            }
+            else
+            {
+                return ( 2* (int(s[1]) - int(s[s.size()-2]) + 23) % 23  );
+            }
+        }
+    }
+
+    string codeToString(char x)
+    {
+        return (table[int(x)]);
+    }
+
+};
+
 int main() {
 
    Minify();
