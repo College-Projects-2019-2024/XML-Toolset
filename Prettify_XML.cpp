1f9889380d66ac038ea62d2e43e6c9f33aa11d91
mohamed ahmed samir <105310173+Mohamed-samir-504@users.noreply.github.com>
2022-11-19 10:26:30 +0200

    finished prettify logic

--- Prettify_XML.cpp
+++ Prettify_XML.cpp
@@ -1,4 +1,4 @@
-//prettify partially done, it works correctly with tags only and not in all cases.
+//prettify done
 
 
 
@@ -6,7 +6,6 @@
 #include "string"
 #include "stack"
 #include "vector"
-
 using namespace std;
 
 vector<string>answer;
@@ -20,52 +19,63 @@ string insert_tab(string s,int n){
     return s;
 }
 
-void prettify(){
+void prettify() {
     freopen("In.in","r",stdin);
-
     string current_line;
     stack<int> stac;
     int count = 0;
     string str;
-    while(getline(cin, current_line)){
+    while (getline(cin, current_line)) {
 
-        while(current_line[0] == ' '){
-            current_line.erase(0,1);
+        while (current_line[0] == ' ') {
+            current_line.erase(0, 1);
         }
-//Get tag nameSS
+
         int position = 0;
         int x = current_line.length() - 1;
 
-        while (position != string::npos && position != x) {
+        while ( position != x) {
 
             int first = current_line.find('<', position);
-            position = current_line.find('>', position + 1);
+            position = current_line.find('>', first);
+            int next = current_line.find('<',position);
 
-            if (position == -1 && first == -1) break;
-        }
 
-        if(current_line[0] == '<' && current_line[1] != '/'){
-            if(current_line.find('/') != -1){
-                answer.push_back(insert_tab(current_line,count));
+
+            if ( first == -1 && position == -1){
+                answer.push_back(insert_tab(current_line, count));
+                break;
             }
-            else{
+
+            else if (current_line[first] == '<' && current_line[first+1] != '/') {
+
                 stac.push(count);
-                answer.push_back(insert_tab(current_line,count));
+                str = current_line.substr(first,position-first+1);
+                answer.push_back(insert_tab(str, count));
+                if(next-position-1 >0){
+                    if(next == -1){
+                        str = current_line.substr(position+1,x-position);
+                        answer.push_back(insert_tab(str, count+1));
+                        position = x;
+                    }
+                    else{
+                        str = current_line.substr(position+1,next-position-1);
+                        answer.push_back(insert_tab(str, count+1));
+                    }
+
+                }
                 count++;
+
+            }
+            else if (current_line[first] == '<' && current_line[first+1] == '/') {
+                str = current_line.substr(first,position-first+1);
+                answer.push_back(insert_tab(str, stac.top()));
+                stac.pop();
+                count--;
             }
 
         }
-        else if(current_line[0] == '<' && current_line[1] == '/'){
-            answer.push_back(insert_tab(current_line,stac.top()));
-            stac.pop();
-            count--;
-        }
-        else{
-            for(int i = 0; i<current_line.size()/50; i+=50)
-                str = current_line.substr(i,50);
-            answer.push_back(insert_tab(str,stac.top()+1));
-        }
-
     }
+
     for(auto & i : answer) cout<<i<<endl;
 }
