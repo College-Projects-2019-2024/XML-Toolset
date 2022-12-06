aff4d4a2b37aaac8f31ed09e24518934a985e70c
mohamed ahmed samir <105310173+Mohamed-samir-504@users.noreply.github.com>
2022-12-06 20:53:12 +0200

    updated header file

--- Prettify_XML.h
+++ Prettify_XML.h
@@ -1,9 +1,11 @@
 #include "string"
+#include "vector"
 using namespace std;
 
 #ifndef COLLEGEPROJECT_PRETTIFY_XML_H
 #define COLLEGEPROJECT_PRETTIFY_XML_H
 void prettify(const string& fileName);
 string insert_tab(string s,int n);
+vector<string>divide_string(string str1);
 
 #endif //COLLEGEPROJECT_PRETTIFY_XML_H
