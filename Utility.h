69f8ce8c9ce5ded9601b13ac8f5d4d8d96f572be
mohamed ahmed samir <105310173+Mohamed-samir-504@users.noreply.github.com>
2022-12-09 13:06:38 +0200

    added utility class

---
+++ Utility.h
@@ -0,0 +1,9 @@
+#ifndef COLLEGEPROJECT_UTILITY_H
+#define COLLEGEPROJECT_UTILITY_H
+#include <string>
+using namespace std;
+
+string removeSpacesFromLine(string s);
+vector<string> get_xml(string inputFileName);
+
+#endif //COLLEGEPROJECT_UTILITY_H
