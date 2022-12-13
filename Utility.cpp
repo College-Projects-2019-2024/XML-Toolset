b90972cadf528c3d6aa6e94cb173c4b3a1099c8e
mohamed ahmed samir <105310173+Mohamed-samir-504@users.noreply.github.com>
2022-12-13 11:59:27 +0200

    fixed some errors

--- Utility.cpp
+++ Utility.cpp
@@ -1,39 +1,36 @@
 #include <bits/stdc++.h>
 #include "Utility.h"
 #include "queue"
+
 using namespace std;
 ifstream fileInputStream2;
 
-string insert_tab(string s,int n){
+string insert_tab(string s, int n) {
     string str = " ";
 
-    for(int i = 0; i<4*n; i++){
-        s.insert(0,str);
+    for (int i = 0; i < 4 * n; i++) {
+        s.insert(0, str);
     }
     return s;
 }
-string removeSpacesFromLine(string s)
-{
-    int begin=0 , end = s.length()-1;
 
-    for(int i = begin; i<s.size(); i++)
-    {
-        if(s[i]!=' ')
-        {
+string removeSpacesFromLine(string s) {
+    int begin = 0, end = s.length() - 1;
+
+    for (int i = begin; i < s.size(); i++) {
+        if (s[i] != ' ') {
             begin = i;
             break;
         }
     }
 
-    for(int i = end; i>=0; i--)
-    {
-        if(s[i]!=' ')
-        {
+    for (int i = end; i >= 0; i--) {
+        if (s[i] != ' ') {
             end = i;
             break;
         }
     }
-    return s.substr(begin,end+1);
+    return s.substr(begin, end + 1);
 }
 
 vector<Line> get_xml(string inputFileName) {
@@ -43,11 +40,13 @@ vector<Line> get_xml(string inputFileName) {
     string str;
     string s = "";
     vector<Line> xml;
-    //a queue to store any text other than tags
-    queue<string>q;
+    //a queue to store any text(other than tags) and its line number
+    queue<pair<string, int>> q;
     int idx = 0;
 
+
     while (getline(fileInputStream2, current_line)) {
+        //current line number
         idx++;
         //remove spaces from start on the line
         while (current_line[0] == ' ') {
@@ -62,9 +61,9 @@ vector<Line> get_xml(string inputFileName) {
         int x = current_line.length() - 1;
 
         //if line is empty
-        if(x == -1)continue;
-        if(position == x){
-            xml.push_back({removeSpacesFromLine(current_line),idx});
+        if (x == -1)continue;
+        if (position == x) {
+            xml.push_back({removeSpacesFromLine(current_line), idx});
             continue;
         }
 
@@ -79,53 +78,84 @@ vector<Line> get_xml(string inputFileName) {
             //if the line consists of text at start then closing tag
             if (d < first && text_at_start) {
                 str = current_line.substr(d, first);
-                q.push(str);
+                q.push({str, idx});
+
                 text_at_start = false;
+
             }
-                //if the line consists only of text
-            else if (first == -1 && position == -1) {
-                q.push(current_line);
+            //if the line consists only of text
+            if (first == -1 && position == -1) {
+                q.push({current_line, idx});
                 position = x;
+
             }
-                //found an opening tag
-            else if (current_line[first] == '<' && current_line[first + 1] != '/') {
+            //found an opening tag
+            if (current_line[first] == '<' && current_line[first + 1] != '/') {
+                //this means there is text that has not been pushed to the xml vector yet
+                if (!q.empty()) {
+                    int temp = q.front().second;
+                    while (!q.empty()) {
+                        s += q.front().first;
+                        q.pop();
+
+                    }
+                    xml.push_back({removeSpacesFromLine(s), temp});
+                    s = "";
+
+                }
                 text_at_start = false;
                 str = current_line.substr(first, position - first + 1);
-                xml.push_back({removeSpacesFromLine(str),idx});
+                xml.push_back({removeSpacesFromLine(str), idx});
 
                 //if the line consists of an opening tag then text
                 if (next == -1 && x - position != 0) {
                     str = current_line.substr(position + 1, x - position);
-                    q.push(str);
+                    q.push({str, idx});
                     position = x;
                 }
                 //if the line consists of an opening tag then text then closing tag
                 if (next - position - 1 > 0) {
 
                     str = current_line.substr(position + 1, next - position - 1);
-                    q.push(str);
-
+                    q.push({str, idx});
 
                 }
             }
-                //found a closing tag
-             if (current_line[first] == '<' && current_line[first + 1] == '/') {
-                 //this means there is text that has not been pushed to the xml vector yet
-                if(!q.empty()){
-                    while(!q.empty()){
-                        s+=q.front();
+            //found a closing tag
+            if (current_line[first] == '<' && current_line[first + 1] == '/') {
+                //this means there is text that has not been pushed to the xml vector yet
+                if (!q.empty()) {
+                    int temp = q.front().second;
+                    while (!q.empty()) {
+                        s += q.front().first;
                         q.pop();
+
                     }
-                    xml.push_back({removeSpacesFromLine(s),idx});
-                    s="";
+
+                    xml.push_back({removeSpacesFromLine(s), temp});
+                    s = "";
+
                 }
 
                 str = current_line.substr(first, position - first + 1);
-                xml.push_back({removeSpacesFromLine(str),idx});
+                xml.push_back({removeSpacesFromLine(str), idx});
                 text_at_start = false;
+                //if the line consists of an closing tag then text
+                if (next == -1 && x - position != 0) {
+                    str = current_line.substr(position + 1, x - position);
+                    q.push({str, idx});
+                    position = x;
+                }
 
+                if (next - position - 1 > 0) {
 
+                    str = current_line.substr(position + 1, next - position - 1);
+                    q.push({str, idx});
+
+                }
             }
+
+
         }
 
     }
