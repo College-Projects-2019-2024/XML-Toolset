fddffd06d42dd887a738c8c9b104510f679f9f6b
mohamed ahmed samir <105310173+Mohamed-samir-504@users.noreply.github.com>
2022-12-15 20:27:56 +0200

    Update Utility.h

--- Utility.h
+++ Utility.h
@@ -1,7 +1,30 @@
 #ifndef COLLEGEPROJECT_UTILITY_H
 #define COLLEGEPROJECT_UTILITY_H
 #include <string>
+#define fori(size) for(int i=0; i < (size); i+= 1)
+
 using namespace std;
+struct Line{
+    string text;
+    int index;
+};
+
+class treeNode{
+public:
+    int max;
+    string type;
+    string text;
+    vector<treeNode *> children;
+
+    treeNode(int max, string type,string text, vector<treeNode *> children)
+    {
+        this->max = max;
+        this->type = type;
+        this->text = text;
+        this->children = children;
+    }
+};
+
 struct Line{
     string text;
     int index;
@@ -10,5 +33,5 @@ string insert_tab(string s,int n);
 string removeSpacesFromLine(string s);
 vector<Line> get_xml(string inputFileName);
 vector<string> line_to_str(vector<Line>v);
-
+treeNode* ans(treeNode* usersSample);
 #endif //COLLEGEPROJECT_UTILITY_H
