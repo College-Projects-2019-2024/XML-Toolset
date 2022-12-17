56a8a63c7889127bb074186868c16c8d09fff85d
marcosherif <68437948+marcosherif@users.noreply.github.com>
2022-12-17 23:06:39 +0200

    Styling

---
+++ xml/build-xml_toolset-Desktop_Qt_6_4_1_MinGW_64_bit-Debug/ui_mainwindow.h
@@ -0,0 +1,189 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 6.4.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTextBrowser>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralwidget;
+    QPushButton *pushButton;
+    QPushButton *pushButton_2;
+    QPushButton *pushButton_3;
+    QPushButton *pushButton_4;
+    QPushButton *pushButton_5;
+    QPushButton *pushButton_6;
+    QPushButton *pushButton_7;
+    QPushButton *pushButton_8;
+    QPushButton *pushButton_9;
+    QTextBrowser *textBrowser;
+    QTextBrowser *textBrowser_2;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName("MainWindow");
+        MainWindow->resize(1208, 707);
+        MainWindow->setStyleSheet(QString::fromUtf8("background-color: #264653;"));
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName("centralwidget");
+        pushButton = new QPushButton(centralwidget);
+        pushButton->setObjectName("pushButton");
+        pushButton->setGeometry(QRect(50, 30, 151, 51));
+        QFont font;
+        font.setPointSize(12);
+        font.setBold(true);
+        pushButton->setFont(font);
+        pushButton->setStyleSheet(QString::fromUtf8("QPushButton#pushButton {\n"
+"border-radius: 25px;\n"
+"background:  #2a9d8f;\n"
+"padding: 6px;\n"
+"color: #e9c46a;\n"
+"}"));
+        pushButton_2 = new QPushButton(centralwidget);
+        pushButton_2->setObjectName("pushButton_2");
+        pushButton_2->setGeometry(QRect(1020, 280, 111, 51));
+        QFont font1;
+        font1.setPointSize(11);
+        font1.setBold(true);
+        pushButton_2->setFont(font1);
+        pushButton_2->setAutoFillBackground(false);
+        pushButton_2->setStyleSheet(QString::fromUtf8("QPushButton#pushButton_2 {\n"
+"border-radius: 25px;\n"
+"background: #e9c46a;\n"
+"padding: 6px;\n"
+"}"));
+        pushButton_3 = new QPushButton(centralwidget);
+        pushButton_3->setObjectName("pushButton_3");
+        pushButton_3->setGeometry(QRect(850, 30, 111, 51));
+        pushButton_3->setFont(font);
+        pushButton_3->setStyleSheet(QString::fromUtf8("QPushButton#pushButton_3 {\n"
+"border-radius: 25px;\n"
+"background:  #2a9d8f;\n"
+"padding: 6px;\n"
+"color: #e9c46a;\n"
+"}"));
+        pushButton_4 = new QPushButton(centralwidget);
+        pushButton_4->setObjectName("pushButton_4");
+        pushButton_4->setGeometry(QRect(1020, 365, 111, 51));
+        pushButton_4->setFont(font1);
+        pushButton_4->setStyleSheet(QString::fromUtf8("QPushButton#pushButton_4 {\n"
+"border-radius: 25px;\n"
+"background: #e9c46a;\n"
+"padding: 6px;\n"
+"}"));
+        pushButton_5 = new QPushButton(centralwidget);
+        pushButton_5->setObjectName("pushButton_5");
+        pushButton_5->setGeometry(QRect(1020, 450, 111, 51));
+        QFont font2;
+        font2.setPointSize(10);
+        font2.setBold(true);
+        pushButton_5->setFont(font2);
+        pushButton_5->setStyleSheet(QString::fromUtf8("QPushButton#pushButton_5 {\n"
+"border-radius: 25px;\n"
+"background: #e9c46a;\n"
+"padding: 6px;\n"
+"}"));
+        pushButton_6 = new QPushButton(centralwidget);
+        pushButton_6->setObjectName("pushButton_6");
+        pushButton_6->setGeometry(QRect(1020, 535, 111, 51));
+        QFont font3;
+        font3.setPointSize(13);
+        font3.setBold(true);
+        pushButton_6->setFont(font3);
+        pushButton_6->setStyleSheet(QString::fromUtf8("QPushButton#pushButton_6 {\n"
+"border-radius: 25px;\n"
+"background: #e9c46a;\n"
+"padding: 6px;\n"
+"}"));
+        pushButton_7 = new QPushButton(centralwidget);
+        pushButton_7->setObjectName("pushButton_7");
+        pushButton_7->setGeometry(QRect(1020, 195, 111, 51));
+        QFont font4;
+        font4.setBold(true);
+        pushButton_7->setFont(font4);
+        pushButton_7->setStyleSheet(QString::fromUtf8("QPushButton#pushButton_7 {\n"
+"border-radius: 25px;\n"
+"background: #e9c46a;\n"
+"padding: 6px;\n"
+"}"));
+        pushButton_8 = new QPushButton(centralwidget);
+        pushButton_8->setObjectName("pushButton_8");
+        pushButton_8->setGeometry(QRect(1020, 110, 111, 51));
+        pushButton_8->setFont(font4);
+        pushButton_8->setStyleSheet(QString::fromUtf8("QPushButton#pushButton_8 {\n"
+"border-radius: 25px;\n"
+"background: #e9c46a;\n"
+"padding: 6px;\n"
+"}"));
+        pushButton_9 = new QPushButton(centralwidget);
+        pushButton_9->setObjectName("pushButton_9");
+        pushButton_9->setGeometry(QRect(1020, 620, 111, 51));
+        pushButton_9->setFont(font2);
+        pushButton_9->setStyleSheet(QString::fromUtf8("QPushButton#pushButton_9 {\n"
+"border-radius: 25px;\n"
+"background: #e9c46a;\n"
+"padding: 6px;\n"
+"}"));
+        textBrowser = new QTextBrowser(centralwidget);
+        textBrowser->setObjectName("textBrowser");
+        textBrowser->setGeometry(QRect(20, 110, 431, 561));
+        textBrowser->setStyleSheet(QString::fromUtf8("QTextBrowser#textBrowser{\n"
+"background-color: #f4a261;\n"
+"}"));
+        textBrowser_2 = new QTextBrowser(centralwidget);
+        textBrowser_2->setObjectName("textBrowser_2");
+        textBrowser_2->setGeometry(QRect(460, 110, 531, 561));
+        textBrowser_2->setStyleSheet(QString::fromUtf8("QTextBrowser#textBrowser_2{\n"
+"background-color: #f4a261;\n"
+"}"));
+        MainWindow->setCentralWidget(centralwidget);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName("statusbar");
+        MainWindow->setStatusBar(statusbar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        pushButton->setText(QCoreApplication::translate("MainWindow", "Choose a file", nullptr));
+        pushButton_2->setText(QCoreApplication::translate("MainWindow", "Prettify", nullptr));
+        pushButton_3->setText(QCoreApplication::translate("MainWindow", "Save file", nullptr));
+        pushButton_4->setText(QCoreApplication::translate("MainWindow", "Compress", nullptr));
+        pushButton_5->setText(QCoreApplication::translate("MainWindow", "Decompress", nullptr));
+        pushButton_6->setText(QCoreApplication::translate("MainWindow", "Minify", nullptr));
+        pushButton_7->setText(QCoreApplication::translate("MainWindow", "Detect errors", nullptr));
+        pushButton_8->setText(QCoreApplication::translate("MainWindow", "Correct errors", nullptr));
+        pushButton_9->setText(QCoreApplication::translate("MainWindow", "To JSON", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
